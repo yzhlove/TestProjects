c827a70355705dd1aa27a03bca05def0a48fff98
余子涵@xianjinchaoren.cn <>
2018-01-26 23:07:37 +0800

    pub_sub

---
+++ Test_17/pub.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2018/1/26
+ * Time: 上午11:37
+ */
+/* redis 按模式发布以及订阅 */
+
+
---
+++ Test_17/sub.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2018/1/26
+ * Time: 上午11:37
+ */
+/* redis 按模式发布以及订阅 */
