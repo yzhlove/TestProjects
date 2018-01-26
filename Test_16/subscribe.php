5dbe4bec373cf78b41b8b2c2ba0892b2ed3fc8a1
余子涵@xianjinchaoren.cn <>
2018-01-26 10:50:40 +0800

    change

--- Test_16/subscribe.php
+++ Test_16/subscribe.php
@@ -7,14 +7,21 @@
  */
 /* redis 发布订阅  */
 
+$redis = new Redis();
+$redis->connect("127.0.0.1",6379);
+
 function handlerMessage($instance,$channel,$message) {
     var_dump($instance);
     var_dump($channel);     //消息类型
     var_dump($message);     //消息内容
-}
 
-$redis = new Redis();
-$redis->connect("127.0.0.1",6379);
+    global $redis;
+
+    $arr = explode("-",$message);
+    if ($arr[0] == "16") {
+        echo "phpredis 没有取消订阅这个方法\n";
+    }
+}
 
 /* 订阅消息 */
 $channel = ['cctv'];
