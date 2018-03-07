b301779e7c09a5c296fe7a57d8352580d5e432b7
余子涵@xianjinchaoren.cn <>
2018-03-07 09:10:48 +0800

    debug

---
+++ Test_27/test8.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2018/3/6
+ * Time: 下午10:03
+ */
+
+$user = 0;
+echo "user = $user";
+$user->sid = 12;
+echo "user-sid = " . $user->sid;
