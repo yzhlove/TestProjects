1b3cadedda14001d1656943f8ea98ccdb61b39ac
余子涵@xianjinchaoren.cn <>
2018-03-13 13:39:26 +0800

    8

--- Test_27/test8.php
+++ Test_27/test8.php
@@ -6,7 +6,18 @@
  * Time: 下午10:03
  */
 
-$user = 0;
-echo "user = $user";
-$user->sid = 12;
-echo "user-sid = " . $user->sid;
+//$user = 0;
+//echo "user = $user";
+//$user->sid = 12;
+//echo "user-sid = " . $user->sid;
+
+//for ($i = 0;$i < 10;$i++) {
+//    $number = mt_rand(0,30);
+//    echo "number = " . $number . "\n";
+//}
+
+$current_time = time();
+sleep(2);
+$last_time = time();
+echo "current_time = " . $current_time . " _ last_time = " . $last_time . "\n";
+echo $last_time - $current_time;
