37c25c5dd81f0366638a5a671dbc1ba724bc6cf2
花江夏树 <love@huajiangxiashudeMacBook-Pro.local>
2018-05-27 15:35:06 +0800

    yzh

---
+++ Test_30/03.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: love
+ * Date: 2018/5/27
+ * Time: 下午3:33
+ */
+interface Inter {
+
+    public function add() ;
+    public function mulit() ;
+
+}
+
+
+class Test implements Inter {
+
+    public function add()
+    {    // TODO: Implement add() method.
+    }
+
+    public function mulit()
+    {   // TODO: Implement mulit() method.
+    }
+}
