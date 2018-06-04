63a8aaa8de74b5cfbae84987bc7486bb3add9bec
花江夏树 <yuzihan@xianjinchaoren.cn>
2018-06-04 20:50:43 +0800

    yzh

--- Test_38/07.php
+++ Test_38/07.php
@@ -6,26 +6,47 @@
  * Time: 下午7:24
  */
 
-class TParent {
+abstract class TParent {
 
     public function __construct()
     {
+
+    }
+
+    public function start() {
         $this->init();
     }
 
-    public function init() {
-        echo "parent::init";
-    }
+    public abstract function init() ;
 
 }
 
 class TChild extends TParent {
 
+    public function __construct()
+    {
+        parent::__construct();
+        echo "\nchild::construct";
+    }
+
     public function init()
     {
-        echo "child::init";
+        echo "\nchild::init";
     }
 
 }
 
+class CChild extends TParent {
+
+    public function init()
+    {
+        // TODO: Implement init() method.
+        echo "\ncchilder::init\n";
+    }
+}
+
 $child = new TChild();
+$child->start();
+
+$ct = new CChild();
+$ct->start();
