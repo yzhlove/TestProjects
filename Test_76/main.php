d26a988cd51159d78be39b475ba5e27adb563802
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-01-25 11:12:09 +0800

    no

---
+++ Test_76/main.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: love
+ * Date: 2019-01-08
+ * Time: 14:29
+ */
+
+
+$a = 1;
+$b = 2;
+$c = 3;
+
+$c = $a + $b;
+
+echo $c ;
---
+++ Test_77/main.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: love
+ * Date: 2019-01-08
+ * Time: 14:44
+ * @param int $a
+ * @param int $b
+ * @return int
+ */
+
+
+function add(int $a , int $b) : int {
+    return $a + $b;
+}
+
+$a = 3;
+$b = 5;
+echo add($a,$b);
+
